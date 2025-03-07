09ba969f836b5d1ec3742b265d434176bb4b2899
MothtatoSoup <maddy.puryear@outlook.com>
2025-03-07 13:24:59 -0500

    pick up at page 183

--- js05.js
+++ js05.js
@@ -4,11 +4,86 @@
       Chapter Case
 
       Application to generate a slide show
-      Author: 
-      Date:   
+      Author: maddy
+      Date:   march 7 25
 
       Filename: js05.js
 */
+window.addEventListener("load", createLightbox);
+
+function createLightbox() {
+   // lbox container
+   let lightbox = document.getElementById("lightbox");
+
+   // lbox parts
+   let lbTitle = document.createElement("h1");
+   let lbCounter = document.createElement("div");
+   let lbPrev = document.createElement("div");
+   let lbNext = document.createElement("div");
+   let lbPlay = document.createElement("div");
+   let lbImages = document.createElement("div");
+
+   // design title
+   lightbox.appendChild(lbTitle);
+   lbTitle.id = "lbTitle";
+   lbTitle.textContent = lightboxTitle;
+
+   // des slide count
+   lightbox.appendChild(lbCounter);
+   lbCounter.id = "lbCounter";
+   let currentImg = 1;
+   lbCounter.textContent = currentImg + "/" + imgCount;
+
+   // des prev button
+   lightbox.appendChild(lbPrev);
+   lbPrev.id = "lbPrev";
+   lbPrev.innerHTML = "&#9664"
+   lbPrev.onclick = showPrev;
+
+   // des next button
+   lightbox.appendChild(lbNext);
+   lbNext.id = "lbNext";
+   lbNext.innerHTML = "&#9654"
+   lbNext.onclick = showNext;
+
+   // des play/pause butt
+   lightbox.appendChild(lbPlay);
+   lbPlay.id = "lbPlay";
+   lbPlay.innerHTML = "&#9199"
+   let timeID;
+   lbPlay.onclick = function(){
+      showNext();
+      timeID = window.setInterval(showNext, 1500);
+   }
+
+   // des images contain
+   lightbox.appendChild(lbImages);
+   lbImages.id = "lbImages";
+
+   // ad imgs from array
+   for (let i = 0; i < imgCount; i++){
+      let image = document.createElement("img");
+      image.src = imgFiles[i];
+      image.alt = imgCaptions[i];
+      lbImages.appendChild(image);
+   }
+}
+
+// func to move thru img list
+function showNext(){
+   lbImages.appendChild(lbImages.firstElementChild);
+   (currentImg < imgCount) ? currentImg++ : currentImg = 1;
+   lbCounter.textContent = currentImg + "/" + imgCount
+}
+
+// func to move backwrds thru list
+function showPrev(){
+   lbImages.insertBefore(lbImages.lastElementChild, lbImages.firstElementChild);
+   (currentImg > 1) ? currentImg-- : currentImg = imgCount;
+   lbCounter.textContent = currentImg + "/" + imgCount;
+}
+
+
 
 window.addEventListener("load", setupGallery);
 
